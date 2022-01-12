5bb0d89a241eb27a7d6e43d7b6a6c17b9a2bb8e6
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-12 10:20:41 -0600

    Test: Department can hire employees and add employee objects to array

--- spec/department_spec.rb
+++ spec/department_spec.rb
@@ -2,7 +2,9 @@ require './lib/employee'
 require './lib/department'
 
 RSpec.describe Department do
-  let(:customer_service) {Department.new("Customer Service")}
+  let (:customer_service) {Department.new("Customer Service")}
+  let (:bobbi) {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})}
+  let (:aaron) {Employee.new({name: "Aaron Tanaka", age: "25", salary: "90000"})}
 
   it 'exists' do
     expect(customer_service).to be_instance_of Department
@@ -12,4 +14,13 @@ RSpec.describe Department do
     expect(customer_service.name).to eq("Customer Service")
     expect(customer_service.employees).to eq([])
   end
-end 
+
+  it 'can hire employees and add to employees array' do
+    expect(customer_service.employees).to eq([])
+
+    customer_service.hire(bobbi)
+    customer_service.hire(aaron)
+
+    expect(customer_service.employees).to eq([bobbi, aaron])
+  end
+end
