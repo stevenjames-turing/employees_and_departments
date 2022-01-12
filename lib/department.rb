dea9fdc902073b079058519cefb91e43c689ce57
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-12 10:25:20 -0600

    Add #expense(amount) to add amount to Department#expenses

--- lib/department.rb
+++ lib/department.rb
@@ -1,14 +1,19 @@
 class Department
 
-  attr_reader :name, :employees
+  attr_reader :name, :employees, :expenses
 
   def initialize(name)
     @name = name
     @employees = []
+    @expenses = 0
   end
 
   def hire(employee)
     @employees << employee
+  end
+
+  def expense(amount)
+    @expenses += amount
   end 
 
 end
