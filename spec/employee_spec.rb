a16a91be027a36e1217dbd71788571f7cbb1046c
Steven James (he/him) <91357724+stevenjames-turing@users.noreply.github.com>
2022-01-12 10:09:06 -0600

    Test: Employee initializes with a name, age, and salary as arguments

---
+++ spec/employee_spec.rb
@@ -0,0 +1,15 @@
+require './lib/employee'
+
+RSpec.describe Employee do
+  let (:bobbi) {Employee.new({name: "Bobbi Jaeger", age: "30", salary: "100000"})}
+
+  it 'exists' do
+    expect(bobbi).to be_instance_of Employee
+  end
+
+  it 'initializes with a name, age, and salary' do
+    expect(bobbi.name).to eq("Bobbi Jaeger")
+    expect(bobbi.age).to eq(30)
+    expect(bobbi.salary).to eq(100000)
+  end
+end
